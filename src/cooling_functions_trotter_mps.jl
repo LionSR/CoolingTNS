165256accde0c3a6581d9844914979e2315371ad
Sirui@MPQ <divergence_lu@icloud.com>
2024-07-31 11:51:18 +0200

    Add move_sites_back parameter to apply function for correct MPS manipulation

--- src/cooling_functions_trotter_mps.jl
+++ src/cooling_functions_trotter_mps.jl
@@ -41,7 +41,7 @@ function evolve_state_trotter(H_sys, gates, ψ, t; Dmax, cutoff, tau)
         ψ_evolved = tdvp(H_sys, -im * tau, ψ_evolved; nsteps=1, reverse_step=false, normalize=true, maxdim=Dmax, cutoff=cutoff, outputlevel=0)
         
         # Apply the pre-computed gates
-        ψ_evolved = apply(gates, ψ_evolved; cutoff=cutoff, maxdim=Dmax)
+        ψ_evolved = apply(gates, ψ_evolved; cutoff=cutoff, maxdim=Dmax, move_sites_back=true)
         
         normalize!(ψ_evolved)
         orthogonalize!(ψ_evolved, 2)
